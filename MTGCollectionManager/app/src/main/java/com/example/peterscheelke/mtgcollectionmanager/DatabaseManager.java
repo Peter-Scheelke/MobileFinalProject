898db4dbd8b7c59495d803cec96e440db7540067
Peter-Scheelke <peter.scheelke@aggiemail.usu.edu>
2016-11-29 21:53:11 -0700

    The now connects to the database
    And it only took five friggin hours . . .

---
+++ MTGCollectionManager/app/src/main/java/com/example/peterscheelke/mtgcollectionmanager/DatabaseManager.java
@@ -0,0 +1,56 @@
+package com.example.peterscheelke.mtgcollectionmanager;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.util.Log;
+
+// A singleton class that accesses the database of MTG cards
+public class DatabaseManager
+{
+    /*
+    // The single instance of the com.example.peterscheelke.mtgcollectionmanager.DatabaseManager class
+    private static DatabaseManager uniqueInstance;
+
+    // The database containing the MTG cards
+    private static final String DATABASE = "mtgdatabase";
+
+    // Used to make sure the com.example.peterscheelke.mtgcollectionmanager.DatabaseManager is correctly synchronized
+    private static Object databaseLock = new Object();
+
+    // Used to access the database of MTG cards
+    private SQLiteOpenHelper helper;
+
+    // The database of MTG cards
+    private SQLiteDatabase database = null;
+
+    // Prevents a default instance of com.example.peterscheelke.mtgcollectionmanager.DatabaseManager
+    // from being created
+    private DatabaseManager(Context context)
+    {
+        helper = new DBHelper(context);
+    }
+
+    // Gets a reference to the single com.example.peterscheelke.mtgcollectionmanager.DatabaseManager object
+    public static DatabaseManager GetManager(Context context)
+    {
+        synchronized (databaseLock)
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new DatabaseManager(context);
+            }
+
+            return uniqueInstance;
+        }
+    }
+
+    public void Test()
+    {
+        synchronized (databaseLock)
+        {
+
+        }
+    }*/
+}
--- MTGCollectionManager/app/src/main/java/com/example/peterscheelke/mtgcollectionmanager/MainActivity.java
+++ MTGCollectionManager/app/src/main/java/com/example/peterscheelke/mtgcollectionmanager/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.peterscheelke.mtgcollectionmanager;
 
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            DataBaseHelper helper = new DataBaseHelper(this);
+            helper.createDataBase();
+            helper.openDataBase();
+            helper.TestStuff();
+            helper.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
