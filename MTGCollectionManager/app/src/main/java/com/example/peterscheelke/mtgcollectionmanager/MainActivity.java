fbbbafcdcf8402d07e445899aaea8005d0c25102
Peter-Scheelke <peter.scheelke@aggiemail.usu.edu>
2016-12-03 22:56:32 -0700

    Converted the search GUI into a fragment
    And it freaking worked somehow!

--- MTGCollectionManager/app/src/main/java/com/example/peterscheelke/mtgcollectionmanager/MainActivity.java
+++ MTGCollectionManager/app/src/main/java/com/example/peterscheelke/mtgcollectionmanager/MainActivity.java
@@ -1,27 +1,41 @@
 package com.example.peterscheelke.mtgcollectionmanager;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.peterscheelke.mtgcollectionmanager.DatabaseManagement.DatabaseManager;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SEARCH_FRAGMENT_KEY = "searchFragmentId";
+    private static final String LIST_FRAGMENT_KEY = "listFragmentId";
+    private static final String CARD_FRAGMENT_KEY = "cardFragmentId";
+
+    private FragmentManager fragmentManager;
+    private SearchFragment searchFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.fragmentManager = getSupportFragmentManager();
 
-        try
-        {
-            DatabaseManager.InitializeManager(this);
-            DatabaseManager manager = DatabaseManager.GetManager();
+        if (savedInstanceState != null) {
+            int searchFragmentId = savedInstanceState.getInt(SEARCH_FRAGMENT_KEY);
+            this.searchFragment = (SearchFragment)fragmentManager.findFragmentById(searchFragmentId);
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
+        else {
+            this.searchFragment = new SearchFragment();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment_frame, searchFragment);
+            fragmentTransaction.commit();
         }
+    }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle icicle) {
+        super.onSaveInstanceState(icicle);
+        icicle.putInt(SEARCH_FRAGMENT_KEY, this.searchFragment.getId());
     }
 }
